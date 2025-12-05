e340dd5dcf3cf0d65cc47e2279c6ab153606a2e8
destamusic <ulfa20040108@gmail.com>
2025-12-05 11:39:10 +0700

    Update CreatePlaylistModal.tsx

--- components/CreatePlaylistModal.tsx
+++ components/CreatePlaylistModal.tsx
@@ -1,1 +1,16 @@
+// hooks/useCreatePlaylistModal.ts
+import { create } from 'zustand';
 
+interface CreatePlaylistModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useCreatePlaylistModal = create<CreatePlaylistModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useCreatePlaylistModal;
