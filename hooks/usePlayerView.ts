944d9e24f1855c2c8e7a8e9fef24616557b3f8b0
destamusic <ulfa20040108@gmail.com>
2025-12-05 07:07:25 +0700

    Add Zustand store for player view management

--- hooks/usePlayerView.ts
+++ hooks/usePlayerView.ts
@@ -1,1 +1,15 @@
+import { create } from 'zustand';
 
+interface PlayerViewStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const usePlayerView = create<PlayerViewStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default usePlayerView;
