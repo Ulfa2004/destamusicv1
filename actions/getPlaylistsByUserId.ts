eb1e1a3e2ddc5feca3d52348f4da24e42718a728
destamusic <ulfa20040108@gmail.com>
2025-12-05 12:28:45 +0700

    Update getPlaylistsByUserId.ts

--- actions/getPlaylistsByUserId.ts
+++ actions/getPlaylistsByUserId.ts
@@ -1,1 +1,31 @@
+// actions/getPlaylistsByUserId.ts
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { Playlist } from '@/types'; 
+
+export const getPlaylistsByUserId = async (): Promise<Playlist[]> => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user) {
+    return []; 
+  }
+
+  const { data, error } = await supabase
+    .from('playlists')
+    .select('id, title, user_id, image_path') 
+    .eq('user_id', user.id) 
+    .order('created_at', { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+  
+  return (data as any) || [];
+};
+
+export default getPlaylistsByUserId;
 
